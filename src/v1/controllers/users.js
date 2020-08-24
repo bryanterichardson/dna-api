c39dde95a637dfb31ea284f3f6f919f5eec9cf97
Eric <bryanterichardson@gmail.com>
2020-08-24 07:57:16 -0700

    Create and Use User Views

--- src/v1/controllers/users.js
+++ src/v1/controllers/users.js
@@ -1,10 +1,13 @@
 import debug from 'debug'
 
 import * as responseCodes from "../../helpers/responseCodes.js"
+import viewHandler from "../../helpers/viewHandler.js"
 import User from '../models/users.js'
+import userView from '../views/users.js'
 
 
 const debug_logger = debug('app:user:controller')
+const viewByRole = viewHandler(userView)
 
 
 export const createUser = async (req, res) => {
@@ -16,6 +19,7 @@ export const createUser = async (req, res) => {
     }
     try {
         const result = await User.create(req.body.email, req.body.password)
+        viewByRole(req.user.role_name, result)
         let user = result[0] || {}
         return responseCodes.status200(res, {user})
     } catch (e) {
@@ -29,6 +33,7 @@ export const deleteUserById = async (req, res) => {
     }
     try {
         const result = await User.deleteById(req.params.id)
+        viewByRole(req.user.role_name, result)
         let user = result[0] || {}
         return responseCodes.status200(res, {user})
     } catch (e) {
@@ -39,6 +44,7 @@ export const deleteUserById = async (req, res) => {
 export const getUserById = async (req, res) => {
     try {
         const result = await User.getById(req.params.id)
+        viewByRole(req.user.role_name, result)
         let user = result[0] || {}
         if (user) { return responseCodes.status200(res, {user}) }
         return responseCodes.status404(res)
@@ -57,6 +63,7 @@ export const getUserLikes = async (req, res) => {
             .getLikesByUserId(req.params.id, req.params.type)
             .orderBy('created_at', 'desc')
             .paginate({perPage: 10, currentPage: page})
+        viewByRole(req.user.role_name, result)
         let likes = result.data || []
         if (likes) { return responseCodes.status200(res, {likes}) }
         return responseCodes.status404(res)
@@ -71,6 +78,7 @@ export const updateUserById = async (req, res) => {
     }
     try {
         const result = await User.updateById(req.params.id, req.body)
+        viewByRole(req.user.role_name, result)
         let user = result[0] || {}
         return responseCodes.status200(res, {user})
     } catch(e) {
---
+++ src/v1/views/users.js
@@ -0,0 +1,12 @@
+const userView = {
+    id: '*',
+    email: ['self', 'admin'],
+    email_verified: ['self', 'admin'],
+    encrypted_password: '-',
+    role_id: ['admin'],
+    can_post: ['admin'],
+    created_at: '*',
+    updated_at: '*'
+}
+
+export default userView
